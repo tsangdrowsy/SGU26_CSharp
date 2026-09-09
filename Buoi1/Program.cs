ed876f5451d7ca1c7e81ec5d6814b436aebdfe49
drowsy <vnthanhsang@gmail.com>
2026-09-09 09:43:21 +0700

    ea sports

---
+++ Buoi1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CSharp.Buoi1
+{
+    class DoiSangGiay
+    {
+        public static void Main(string[] args)
+        {
+            int gio, phut, giay;
+
+            Console.Write("Nhap so gio: ");
+            gio = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so phut: ");
+            phut = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so giay: ");
+            giay =int.Parse(Console.ReadLine());
+
+
+            int tonggiay= gio*3600+phut*60+giay;
+
+            Console.WriteLine("Tong so giay cua {0}:{1}:{2} la: {3}",gio,phut,giay,tonggiay);
+            Console.Read();
+        }
+    }
+}
