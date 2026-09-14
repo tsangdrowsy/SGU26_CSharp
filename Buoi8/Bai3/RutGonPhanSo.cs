6a6af81756191596b2179d13677ab850e6a78e76
justhuy23 <luxubu399@gmai.com>
2026-09-14 16:44:32 +0700

    bibilabu

---
+++ Buoi8/Bai3/RutGonPhanSo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Buoi8.Bai3
+{
+    class RutGonPhanSo
+    {
+    //    static int U(int a, int b)
+    //    {
+    //        if (b == 0) return a;
+    //        return UCLN(b, a % b);
+    //    }
+        static int U(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            Console.Write("Moi ban nhap tu so: ");
+            int tu = int.Parse(Console.ReadLine());
+            Console.Write("Moi ban nhap mau so: ");
+            int mau = int.Parse(Console.ReadLine());
+            int p = U(Math.Abs(tu), Math.Abs(mau));
+            int y  = tu / p;
+            int z = mau / p;
+            Console.WriteLine("Phan so {0}/{1} duoc rut gon thanh {2}/{3}.", tu, mau, y, z);
+        }
+    }
+}
---
+++ Buoi8/Bai4/DemChuSo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Buoi8.Bai4
+{
+    class DemChuSo
+    {
+        static void Main()
+        {
+            Console.Write("Moi ban nhap so nguyen n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] dem = new int[10];
+            int temp = n;
+            if(temp==0)
+            {
+                dem[0]=1;
+            }
+            else
+            {
+            while (temp > 0)
+                {
+                    int chuSo = temp % 10;  
+                    dem[chuSo]++;            
+                    temp /= 10;             
+                }
+            }
+            for (int i = 0; i <= 9; i++)
+            {
+                if (dem[i] > 0)
+                    Console.WriteLine("Chu so {0} xuat hien {1} lan.", i, dem[i]);
+            }
+        }
+    }
+}
