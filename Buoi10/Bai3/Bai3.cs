abae1b3af8a9667936d19b3e99ef2b5621a78e51
drowsy <vnthanhsang@gmail.com>
2026-09-14 23:57:22 +0700

    guh, am done

---
+++ Buoi10/Bai3/Bai3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+
+namespace Buoi10.Bai1
+{
+    class DayDanDau
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Nhap so phan tu n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] e = new int[n]; //making a fixed array following the number size
+
+            for (int i = 0; i < e.Length; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                e[i] = temp;
+
+            }
+            int dem = 0;
+
+            for (int i = 0; i < e.Length - 1; i++) //I love the bare bone brute forcing
+            {
+                if (e[i] > 0)
+                {
+                    if (e[i + 1] < 0)
+                    {
+                        dem++;
+                    }
+                }
+                if (e[i] < 0)
+                {
+                    if (e[i + 1] > 0)
+                    {
+                        dem++;
+                    }
+                }
+            }
+            Console.WriteLine("Day so co {0} phan tu: ", n);
+
+            for (int i = 0; i < e.Length; i++)
+            {
+                int xd = 0;
+                xd = e[i];
+                Console.Write("{0} ", xd);
+            }
+            if (dem == n-1)
+            {
+                Console.WriteLine("Day so tren la day dan dau ");
+            }
+
+            else
+            {
+                Console.WriteLine("mmb");
+            }
+
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ Buoi10/Bai5/Bai5.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data;
+using System.Reflection.Metadata;
+
+
+namespace Buoi10.Bai1
+{
+    class DayDanDau
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Nhap so phan tu n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] e = new int[n]; //making a fixed array following the number size
+
+            for (int i = 0; i < e.Length; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                e[i] = temp;
+
+            }
+
+            Console.WriteLine("Day so co {0} phan tu: ", n);
+
+            for (int i = 0; i < e.Length; i++)
+            {
+                int xd = 0;
+                xd = e[i];
+                Console.Write("{0} ", xd);
+            }
+
+        
+            int max = 0;
+            int demm = 0;
+            int begin = 0;
+            int end = 0;
+            int[] x = new int[0];
+
+            for (int i = 0; i < e.Length; i++)
+            {
+                if (e[i] > 0)
+                {
+
+                    demm++;
+
+                    if (demm > max)
+                    {
+                        max = demm;
+                        end = i;
+                        begin = i - max + 1;
+
+
+                        Array.Resize(ref x, max);
+
+                        int iX = 0;
+
+                        for (int j = begin; j <= end; j++)
+                        {
+                            x[iX] = e[j];
+                            iX++;
+                        }
+
+                    }
+
+                }
+
+                else
+                {
+                 
+                    demm = 0;
+
+                }
+
+            }
+
+
+
+            Console.WriteLine("Doan duong dai nhat[{0},{1}] dai nhat:", begin, end);
+
+            for (int j = 0; j < x.Length; j++)
+            {
+                Console.Write("{0} ", x[j]);
+            }
+
+
+            Console.Read();
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
